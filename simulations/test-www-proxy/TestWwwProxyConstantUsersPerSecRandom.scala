e8aff83ef2011fae79f8b7306a77957c2e308e9c
William Belle <william.belle@gmail.com>
2018-03-08 15:44:50 +0100

    group and assert sim

--- simulations/test-www-proxy/TestWwwProxyConstantUsersPerSecRandom.scala
+++ simulations/test-www-proxy/TestWwwProxyConstantUsersPerSecRandom.scala
@@ -1,5 +1,6 @@
 /**
- * (c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2018
+ * (c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE,
+ * Switzerland, VPSI, 2018
  */
 package computerdatabase
 
@@ -9,22 +10,38 @@ import scala.concurrent.duration._
 
 class TestWwwProxyConstantUsersPerSecRandom extends Simulation {
 
+  // Here is the root for all relative URLs
+  val baseUrl = "https://test-www-proxy.epfl.ch"
+
+  // Here are the common headers
+  val header  = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+
   val httpConf = http
-    .baseURL("https://test-www-proxy.epfl.ch") // Here is the root for all relative URLs
+    .baseURL(baseUrl)
     .inferHtmlResources()
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .acceptHeader(header)
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
-    .userAgentHeader("Gatling Test - Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    .userAgentHeader("IDevelopBot - v1.0.0")
+    .disableCaching
+    .disableClientSharing
+    .maxConnectionsPerHostLikeChrome
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("test-www-proxy.epfl.ch/index.fr.html")
-      .get("/index.fr.html"))
+  // A scenario is a chain of requests and pauses
+  val scn = scenario("test-www-proxy").group("homepage") {
+    exec(http("index.fr.html").get("/index.fr.html")
+  )}
 
   setUp(
-    // Injects users at a constant rate, defined in users per second, during a given duration.
+    // Injects users at a constant rate
     // Users will be injected at randomized intervals.
-    scn.inject(constantUsersPerSec(51) during(20) randomized
-  ).protocols(httpConf))
+    scn.inject(
+      constantUsersPerSec(35) during(20) randomized
+    ).protocols(httpConf)
+  ).assertions(
+    details("homepage").responseTime.percentile3.lt(500)
+  ).assertions(
+    details("homepage").failedRequests.percent.is(0)
+  )
 }
