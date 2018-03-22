60d74a6a52667693dfcd0017c53d7fbb121e3aae
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2018-03-22 22:43:21 +0100

    Implements more general and smaller container

---
+++ bin/docker-ep.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# Gatling simulation runner entry point
+# This only clone the simulation source repository and execute the test with the given command
+# There is one mandatory argument: the path of the command for running the simulations
+# Then two options:
+# 1. the /sim directory is mounted from the host
+# 2. the /sim directory is created from an "origin" given in second argument
+
+# ---------------------------- Utility functions
+die() {
+  echo $* > /dev/stderr
+  exit 1
+}
+
+while getopts ":f:" OPT; do
+  [[ $OPTARG =~ ^- ]] && die "Option -$OPT requires an argument."
+  case $OPT in
+    :)
+      die "Option -$OPTARG requires an argument."; ;;
+    f)
+      origin="$OPTARG"; ;;
+  esac
+done
+shift $((OPTIND-1))
+CMDS="$@"
+
+# If origin is given, we assume /sim is not yet present
+if [ -n "$origin" ] ; then
+  [ -d /sim ] && die "/sim directory is already present. Do not mount if you want to start from external origin" 
+  if   [[ "$origin" == "*.git" ]] ; then
+    git clone $origin /sim
+  else
+    die "http simulation server not yet implemented"
+  fi
+else
+  [ -d /sim ] || die "/sim directory is not mounted. Run with docker run -v LOCALDIR:/sim" 
+fi
+
+
+CMDPATHS=""
+for cmd in $CMDS ; do
+  t=""
+  for c in /sim/bin/$cmd /sim/$cmd $cmd ; do
+    if [ -f $c ] ; then
+      t=$c
+    fi
+  done
+  CMDPATHS="$CMDPATHS $t"
+done
+
+for c in $CMDPATHS ; do
+  . $c
+done
--- bin/run_simulations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "========================= Simulation runner with Tests='$*'"
-for t in $* ; do
-  /gatling/bin/gatling.sh -sf /sim/simulations/ -rf /sim/results -m -s computerdatabase.$t
-done
-
-
