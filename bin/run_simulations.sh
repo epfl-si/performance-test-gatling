6fbddf5f8ccda1d39b07fa44c8cb15b3eb40b6dd
William Belle <william.belle@gmail.com>
2018-03-23 11:02:16 +0100

    fix results folder path

--- bin/run_simulations.sh
+++ bin/run_simulations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "========================= Simulation runner with Tests='$*'"
 for t in $* ; do
-  /gatling/bin/gatling.sh -sf /sim/simulations/ -rf /sim/results -m -s epfl.$t
+  /gatling/bin/gatling.sh -sf /sim/simulations/ -rf /sim/gatling/results -m -s epfl.$t
 done
