9ac14052bf8f162716427c8c26e71363ba89b0a9
Giovanni Cangiani <giovanni.cangiani@epfl.ch>
2018-04-17 10:42:23 +0200

    Adds help and support for workers synchronization

--- bin/aws_submit.sh
+++ bin/aws_submit.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
 
+usage() {
+  cat << '__EOF' | sed 's/^    //'
+
+    Start Gatling job on AWS.
+    Options:
+      -n NAME    Job name prefix (actual job name will be NAME_datetime)
+      -c COUNT   Number of job instances to start [1]
+      -t TEST    Name of the gatgling test (class) to start [TestWwwProxy]
+      -p PROFILE Name of the profile in ~/.aws/credentials to use
+      -e         Shortcut for -p gatling-eu
+      -u         Shortcut for -p gatling-us
+      -s SECONDS Period of time between synchronization checks. If present 
+                 triggers syncrhonization. 
+      -S URL     Countdown server address
+      -x SECONDS Timeout for synchronization.
+      -m         Add one to worker clients so they can be triggered manually
+
+__EOF
+}
 
 name="epfl"
 tname="TestWwwProxy"
 asize=1
 profile="gatling-eu"
+syncsrv="http://myslideshot.epfl.ch"
+syncto=3600
+syncint=0
+manstart=0
 
-while getopts ":n:c:t:p:eu" OPT; do
+while getopts ":n:c:t:p:s:S:eumh" OPT; do
   [[ $OPTARG =~ ^- ]] && die "Option -$OPT requires an argument."
   case $OPT in
     :)
@@ -20,29 +43,46 @@ while getopts ":n:c:t:p:eu" OPT; do
     p)
       profile="$OPTARG"; ;;
     e)
-      profile=gatling-eu; ;;
+      profile="gatling-eu"; ;;
     u)
-      profile=gatling-us; ;;
+      profile="gatling-us"; ;;
+    S)
+      syncsrv="$OPTARG"; ;;
+    s)
+      syncint="$OPTARG"; ;;
+    x)
+      syncto="$OPTARG"; ;;
+    m)
+      manstart=1; ;;
     h)
       usage; exit 0; ;;
   esac
 done
 
 job_name="${name}_$(date +%Y%m%d%H%M)"
+
+envs="{name=NAME, value=$job_name}, {name=TESTS, value=$tname}"
+if [[ $syncint != "0" ]] ; then
+  echo "Job synchronization. Setting countdown server for ${job_name}. "
+  let s=$asize+$manstart
+  curl "${syncsrv}/set?id=${job_name}&count=${s}"
+  envs="$envs, {name=SYNC, value=$syncsrv}, {name=CI, value=$syncint}, {name=CTO, value=$syncto}"
+fi
+
 if [ "$asize" == "1" ] ; then
   aws --profile $profile batch submit-job \
       --job-queue gatling --job-definition gatling \
       --job-name "$job_name" \
-      --container-overrides "environment=[{name=NAME, value=$job_name}, {name=TESTS, value=$tname}]"
+      --container-overrides "environment=[$envs]"
 else
   aws --profile $profile batch submit-job \
       --job-queue gatling --job-definition gatling \
       --job-name "$job_name" --array-properties "{\"size\":$asize}" \
-      --container-overrides "environment=[{name=NAME, value=$job_name}, {name=TESTS, value=$tname}]"
+      --container-overrides "environment=[$envs]"
 fi
 
 echo "To check the job status:"
-echo "aws --profile gatling batch list-jobs --job-queue gatling"
+echo "aws --profile $profile batch list-jobs --job-queue gatling"
 sleep 5
 echo "Example:"
 aws --profile gatling batch list-jobs --job-queue gatling
